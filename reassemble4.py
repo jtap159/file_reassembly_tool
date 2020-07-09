317eb65e2f587bd1e4fdefca374b623d19c7fb39
Jeremy Tapia <engjat94@gmail.com>
2020-07-09 09:37:16 -0500

    made reassemble4.py to start building up a solution for handling if the same fragment matches on the left and right of the anchor

--- reassemble4.py
+++ reassemble4.py
@@ -1,7 +1,7 @@
 from urllib.parse import unquote_plus, unquote
 
 
-def find_matches(list_of_fragments, anchor_fragment):
+def find_matches(list_of_fragments, anchor_fragment, verify_left=True, verify_right=True):
     """
     Check how each fragment in the list of fragments fits to the anchor fragment
     """
@@ -9,19 +9,22 @@ def find_matches(list_of_fragments, anchor_fragment):
     info = {"num_of_left_matches": 0, "left_matches": [],
             "num_of_right_matches": 0, "right_matches": []}
     for i, frag in enumerate(list_of_fragments):  # check every fragment to see if they fit to the anchor fragment
+        found_match = False
         for j in range(max_overlap, 2, -1):
             if frag[-j:] == anchor_fragment[:j]:  # check for match left of anchor
                 info["num_of_left_matches"] += 1
                 info["left_matches"].append({"frag": frag, "spliced_frag": frag[:-j]})
-                break
-            elif anchor_fragment[-j:] == frag[:j]:  # check for match right of anchor       ### !!!!  is breaking after finding the left match causing issues? should we allow the right to be checked too
+                found_match = True
+            if anchor_fragment[-j:] == frag[:j]:  # check for match right of anchor       ### !!!!  is breaking after finding the left match causing issues? should we allow the right to be checked too
                 info["num_of_right_matches"] += 1                                           ### !!! i tried having the right be check even if a left match is found at that j value and cause a .remove() later in the code
                 info["right_matches"].append({"frag": frag, "spliced_frag": frag[j:]})
+                found_match = True
+            if found_match:
                 break
     return info
 
 
-def verify_matches(matching_info, list_of_fragments, anchor_fragment):
+def verify_matches(matching_info, list_of_fragments, anchor_fragment, verify_left=True, verify_right=True):
     """
     verify that a match is perfectly compatible
     :param matching_info: [list[dict]] the list of fragment information that matched with the anchor fragment
@@ -34,7 +37,7 @@ def verify_matches(matching_info, list_of_fragments, anchor_fragment):
             temp_anchor_frag = info["frag"]
             temp_list_of_fragments = list_of_fragments.copy()
             temp_list_of_fragments.remove(temp_anchor_frag)
-            compatible_info = find_matches(temp_list_of_fragments, temp_anchor_frag)
+            compatible_info = find_matches(temp_list_of_fragments, temp_anchor_frag, verify_left, verify_right)
             if compatible_info["num_of_right_matches"] == 1 and compatible_info['right_matches'][0]['frag'] == anchor_fragment:
                 return info
             elif compatible_info["num_of_left_matches"] == 1 and compatible_info['left_matches'][0]['frag'] == anchor_fragment:
@@ -77,13 +80,13 @@ def assemble_frags(input_file):
 
 
 if __name__ == "__main__":
-    file_ordered = open("frag_files/chopfile-frags.txt", "r")
-    assemble_frags_ordered = assemble_frags(file_ordered)
-    print(assemble_frags_ordered)
-    file_ordered.close()
-    # file_shuffled = open("frag_files/hello-frags.txt", "r")
-    # assemble_frags_shuffled = assemble_frags(file_shuffled)[0].replace("+", " ")
-    # print(assemble_frags_shuffled)
-    # file_shuffled.close()
+    # file_ordered = open("frag_files/Shake-frags.txt", "r")
+    # assemble_frags_ordered = assemble_frags(file_ordered)
+    # print(assemble_frags_ordered)
+    # file_ordered.close()
+    file_shuffled = open("frag_files/hello-ordered-frags.txt", "r")
+    assemble_frags_shuffled = assemble_frags(file_shuffled)[0].replace("+", " ")
+    print(assemble_frags_shuffled)
+    file_shuffled.close()
 
 
