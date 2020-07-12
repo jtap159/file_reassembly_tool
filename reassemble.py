e7e11887a79b329b924f5e3d7e59e9704724773f
Jeremy Tapia <engjat94@gmail.com>
2020-07-12 03:03:09 -0500

    moved latest updates to reassemble.py and removed reassemble2.py. passed pytest for all docs excluding chopfile

--- reassemble.py
+++ reassemble.py
@@ -1,5 +1,6 @@
 from urllib.parse import unquote_plus, unquote
 from collections import Counter
+import numpy as np
 
 
 def find_matches(list_of_fragments, anchor_fragment, fixed_length
@@ -40,7 +41,6 @@ def find_matches(list_of_fragments, anchor_fragment, fixed_length
                     duplicate_count += 1
                     break
         if duplicate_count == 2:
-            print(f"duplicate found on anchor: {[anchor_fragment]}")
             info["duplicate"] = True
 
     return info
@@ -75,17 +75,12 @@ def verify_matches(matching_info, list_of_fragments, anchor_fragment, fixed_leng
 
 
 def assemble_frags(input_file):
-    """
-    Description:
-    reassemble the overlapping fragmented source text
-    :param input_file: file object to the fragment txt file
-    :return: decoded_frags: [list[str]] the reassembled source text as a string
-    """
     decoded_frags = [unquote_plus(line[:-1]) for line in input_file]
     decoded_frags_lengths = [len(frag) for frag in decoded_frags]
     fixed_substring_len = Counter(decoded_frags_lengths).most_common(1)[0][0]
     first_assemble = True
     while first_assemble:
+        no_matches_info = []
         num_of_fragments = len(decoded_frags)
         for k in range(0, num_of_fragments):
             temp_decoded_frags = decoded_frags.copy()
@@ -96,6 +91,8 @@ def assemble_frags(input_file):
             # that only one right side is found and it is the anchor frag
             verified_left_match = verify_matches(anchor_match_info['left_matches'], decoded_frags.copy(), main_anchor_frag, fixed_substring_len, verify_left=False, verify_right=True)
             verified_right_match = verify_matches(anchor_match_info['right_matches'], decoded_frags.copy(), main_anchor_frag, fixed_substring_len, verify_left=True, verify_right=False)
+            if verified_left_match is None and verified_right_match is None:
+                no_matches_info.append(anchor_match_info)
             if verified_left_match is not None and verified_right_match is not None:
                 # if the same fragment matches on the left and right of the anchor and it is verified on both sides
                 # then move to the next fragment
@@ -113,16 +110,109 @@ def assemble_frags(input_file):
                 decoded_frags.append(combine_frags)
                 break
             elif k == num_of_fragments - 1:
-                print("No more perfect matches can be found")
-                first_assemble = False
+                print("No more perfect matches can be found...checking permutations")
+                assembled_permutations = find_best_combination(no_matches_info, decoded_frags, fixed_substring_len)
+                return assembled_permutations
         if len(decoded_frags) == 1:
             print("Assembly Finished!")
             first_assemble = False
     return decoded_frags
 
 
+def find_left_anchor_index(fragment_info, fragments):
+    for info in fragment_info:
+        if len(info['left_matches']) == 0:
+            return fragments.index(info['anchor'])
+
+
+def create_matching_matrix(fragment_info, fragments):
+    # building to the right, so use the right matches of each fragment info
+    # rows are building to the right
+    # columns are building to the left
+    matching_matrix = np.zeros((len(fragments), len(fragments)), dtype=np.int32)
+    for info in fragment_info:
+        row_index = fragments.index(info['anchor'])
+        column_indexes = [fragments.index(right_match['frag']) for right_match in info['right_matches']]
+        for column_index in column_indexes:
+            matching_matrix[row_index][column_index] = 1
+    return matching_matrix
+
+
+def create_tracker_array(frags_used):
+    # create a binary array to track what fragments have been used
+    tracker_array = np.ones(13, dtype=np.int32)
+    for frag_used in frags_used:
+        tracker_array[frags_used] = 0
+    return tracker_array
+
+
+def find_best_combination(fragments_info, fragments, fixed_len):
+    matching_matrix = create_matching_matrix(fragments_info, fragments)
+    left_anchor_index = find_left_anchor_index(fragments_info, fragments)
+    # use the left anchor as the starting point (first slot)
+    frags_permutation = [left_anchor_index]
+    slot_tracker_array = create_tracker_array(frags_permutation)
+    # now find all the possible matches to the right of the left anchor
+    right_matching_bin = matching_matrix[left_anchor_index, :] * slot_tracker_array
+    right_matching_indices = np.where(right_matching_bin == 1)[0].tolist()
+    # create an empty list of lists for tacking the right matches
+    right_matching_tracker = [[] for i in range(0, len(matching_matrix))]
+    right_matching_tracker[0] = right_matching_indices
+
+    possible_permutations = []
+    rm_index = 0  # right match index
+    add_matches_index = 1  # add the right matches to the right match tracker
+    while len(right_matching_tracker[rm_index]) != 0 and rm_index >= 0:
+        select_frag = right_matching_tracker[rm_index][0]
+        frags_permutation.append(select_frag)
+        slot_tracker_array = create_tracker_array(frags_permutation)
+        right_matching_bin = matching_matrix[select_frag, :] * slot_tracker_array
+        right_matching_indices = np.where(right_matching_bin == 1)[0].tolist()
+        if len(right_matching_indices) == 0:
+            possible_permutations.append(frags_permutation.copy())
+            frags_permutation.pop()  # go back one slot
+            del right_matching_tracker[rm_index][0]
+            while len(right_matching_tracker[rm_index]) == 0:
+                rm_index -= 1
+                add_matches_index -= 1
+                frags_permutation.pop()
+                if rm_index < 0:
+                    break
+                del right_matching_tracker[rm_index][0]
+            continue
+        right_matching_tracker[add_matches_index] = right_matching_indices
+        # pick fragment from the right matching indices
+        rm_index += 1
+        add_matches_index += 1
+    assembled_permutations = assemble_permutations(possible_permutations, fragments, fixed_len, left_anchor_index)
+    return assembled_permutations
+
+
+def assemble_permutations(permutations, fragments, fixed_length, left_anchor_index):
+    # need to add parenthesis check and tab auto-correct
+    # all the fragments should be greater than or equal to the fixed length at this point
+    max_overlap = fixed_length - 1
+    complete_fragment_size = len(fragments)
+    # from the permutations we use the left anchor as the starting point every time assuming we found the left anchor
+    assemblies = []
+    for permutation in permutations:
+        if len(permutation) == complete_fragment_size:
+            assembly = fragments[left_anchor_index]
+            del permutation[0]
+            for i in permutation:
+                for j in range(max_overlap, 2, -1):
+                    if assembly[-j:] == fragments[i][:j]:  # check for match right of anchor
+                        assembly = assembly + fragments[i][j:]
+                        break
+            assemblies.append(assembly)
+
+    return assemblies
+
+
 if __name__ == "__main__":
-    file = open("frag_files/chopfile-frags.txt", "r")
-    assemble_fragments = assemble_frags(file)
-    print(assemble_fragments[0])
+    file = open("frag_files/Shake-frags.txt", "r")
+    # sample_fragments = [frag.replace(" ", "@") for frag in sample_fragments]
+    assembled_perms = assemble_frags(file)
     file.close()
+
+
